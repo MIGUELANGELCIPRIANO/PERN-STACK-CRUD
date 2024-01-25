4410392ce33e9803598fdcf3eec26fbebd1a87fe
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-01-25 17:45:37 -0300

    [TEST]: Postgres Connection

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express')
+const pool = require('../db')
 
 const router = Router()
 
-router.get('/tasks')
+router.get('/tasks', async (req, res) => {
+	const result = await pool.query('SELECT NOW()')
+	console.log(result)
+	res.json(result.rows[0].now)
+})
+
 router.get('/tasks/:id')
 router.post('/tasks')
 router.put('/tasks')
