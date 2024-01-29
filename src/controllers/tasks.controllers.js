b7ed0edb30dbd59d685a7f72f07e9e8c2fab2c20
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-01-29 16:42:00 -0300

    ADD Post Task controller

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,21 +1,33 @@
+const pool = require('../db')
+
 const getAllTasks = async (req, res) => {
-    res.send('retrieve all tasks')
+	res.send('retrieve all tasks')
 }
 
 const getTask = async (req, res) => {
-    res.send('retrieve a task')
+	res.send('retrieve a task')
 }
 
-const createTask = (req, res) => {
-    res.send('create a task')
+const createTask = async (req, res) => {
+	const { title, description } = req.body
+	try {
+		const result = await pool.query(
+			'INSERT INTO task(title, description) VALUES ($1, $2) RETURNING *',
+			[title, description]
+		)
+		console.log(result)
+		res.status(200).json(result.rows[0])
+	} catch (error) {
+		res.status(500).json({ message: error.message })
+	}
 }
 
 const updateTask = (req, res) => {
-    res.send('update a task')
+	res.send('update a task')
 }
 
 const deleteTask = (req, res) => {
-    res.send('delete a task')
+	res.send('delete a task')
 }
 
 module.exports = {
